7a0e5edaea38dc34f88b5731ffe0bfd7428ffdb6
KFischer <klaus.fischer@eloware.com>
2021-12-30 09:17:47 +0100

    Changed order of commands

--- version.sh
+++ version.sh
@@ -1,5 +1,16 @@
 #!/bin/zsh
 
+# Check for version code
+if [ -z "$1" ]
+then
+  echo "No version code supplied"
+  exit 2
+fi
+
+
+# Update version in pubspec
+sed -i "" -e "s%version: .*%version: $1%" pubspec.yaml
+
 # Check if publish would work
 flutter pub publish --dry-run
 if [ $? -ne 0 ]
@@ -16,17 +27,6 @@ CURRENT=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
      echo "Your branch has to be 'master' but is '$CURRENT'"
      exit 1
  fi
-
-
-# Check for version code
-if [ -z "$1" ]
-then
-  echo "No version code supplied"
-  exit 2
-fi
-
-# Update version in pubspec
-sed -i "" -e "s%version: .*%version: $1%" pubspec.yaml
 
 # Git it
 git tag -a V$1 -m "Version $1"
